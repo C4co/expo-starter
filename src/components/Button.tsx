b9fcd00da6f102ed2c204778de7da601ee17926b
Carlos Costa <carllos.nc@gmail.com>
2023-03-15 11:47:30 -0300

    fix button height

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -14,7 +14,7 @@ interface ButtonProps extends TouchableOpacityProps {
 export function Button({ children, loading, ...props }: ButtonProps) {
   if (loading) {
     return (
-      <View className="px-4 h-[50] bg-black rounded-full justify-center items-center w-full">
+      <View className="px-4 h-[45] bg-black rounded-full justify-center items-center w-full">
         <ActivityIndicator color="#FFF" />
       </View>
     )
@@ -23,7 +23,7 @@ export function Button({ children, loading, ...props }: ButtonProps) {
   return (
     <TouchableOpacity
       activeOpacity={0.7}
-      className="px-4 h-[50] justify-center items-center bg-black rounded-full w-full"
+      className="px-4 h-[45] justify-center items-center bg-black rounded-full w-full"
       {...props}
     >
       <Text className="text-base text-white leading-none text-center">
